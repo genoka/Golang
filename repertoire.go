ab0ecd1e0a9a7beb4973267ffad56e6735dbff48
genoka <kalidouba@gmail.com>
2017-02-17 20:01:35 +0100

    Create repertoire.go

---
+++ repertoire.go
@@ -0,0 +1,24 @@
+import(
+    "fmt"
+    "io"
+    "ioutil"
+    )
+type Contact struct{
+ id rune
+ nom string 
+ prenom string
+ age byte
+ fonction string
+ mobile uint64
+ fixe uint64
+ rue string
+ villa string
+ avenue string
+ boitepostale rune
+ email string
+ website string
+ fax uint64
+ pays string
+ ville string
+ geomap []float64
+ }
